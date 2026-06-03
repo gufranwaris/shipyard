a564313d47e9a57aa6f8d631d20fd3caa8a36a92
Gufranwaris <gufranwaris6098@gmail.com>
2026-06-03 15:35:45 +0530

    feat: implements deployment endpoint

--- apps/api-server/src/main/java/com/shipyard/service/ProjectService.java
+++ apps/api-server/src/main/java/com/shipyard/service/ProjectService.java
@@ -2,9 +2,7 @@ package com.shipyard.service;
 
 import com.shipyard.dto.CreateProjectRequest;
 import com.shipyard.dto.ProjectResponse;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface ProjectService {
     public ProjectResponse create(CreateProjectRequest request);
 }
